230760a0b4a807bf2e71f72fe39a6ef94919cc40
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-05-29 08:35:11 -0500

    hide mappings for which you didn't specify a label

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -58,7 +58,7 @@ return {
 				spacing = 3, -- spacing between columns
 				align = "center", -- align columns left, center or right
 			},
-			ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+			ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
 			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 			show_help = true, -- show help message on the command line when the popup is visible
 			triggers = "auto", -- automatically setup triggers
