52f57ee44b0268aee6d70f84071d5e01c40c084c
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-06-04 18:47:57 -0500

    comment for future

--- lua/plugins/lsp/settings/ltex.lua
+++ lua/plugins/lsp/settings/ltex.lua
@@ -4,6 +4,7 @@ return{
 			language =  "en-GB",
       -- language =  "es",
       -- dictionary = {"en-GB":["nvim", "linux"]},
+      -- In markdown file add lang: es, https://valentjn.github.io/ltex/advanced-usage.html#magic-comments
 		},
 	},
   -- filetypes = { "bib", "gitcommit", "markdown", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "text","html", "css", "rust", "python", "java", "javascript" }
