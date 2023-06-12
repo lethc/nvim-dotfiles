4c736747ac6fa982cab318826830dd0b42951db2
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-06-12 08:10:16 -0500

    nightly module added

---
+++ lua/plugins/colorscheme/nightly-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  "Alexis12119/nightly.nvim",
+  cmd = "Telescope colorscheme",
+  lazy = false,
+  config = function()
+    require("nightly").setup({
+      -- color = "red", -- blue, green or red
+      transparent = false,
+      styles = {
+        comments = { italic = true },
+        functions = { italic = false },
+        keywords = { italic = false },
+        variables = { italic = false },
+      },
+      highlights = {
+        -- add or override highlights
+        -- Normal = { bg = "#000000" }
+      },
+    })
+  end,
+}
