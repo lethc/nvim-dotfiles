241269fae6395ec3f4556e40e459eec96e156e7a
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-06-12 13:58:32 -0500

    downgrade

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,5 @@
 return {
 	{
-		"renerocksai/telekasten.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"renerocksai/calendar-vim",
-		},
-		config = function()
-			require("telekasten").setup({
-				home = vim.fn.expand("~/Sync"), -- Put the name of your notes directory here
-			})
-		end,
-	},
-	{
 		"xorid/asciitree.nvim",
 		ft = "markdown",
 		config = function()
