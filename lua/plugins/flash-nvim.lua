6e332e75358719d85f8e3ed1821e0129f54d611e
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-07-20 10:00:24 -0500

    flash nvim

--- lua/plugins/flash-nvim.lua
+++ lua/plugins/flash-nvim.lua
@@ -45,33 +45,31 @@ return {
 			-- automatically jump when there is only one match
 			autojump = false,
 		},
-		highlight = {
-			label = {
-				-- add a label for the first match in the current window.
-				-- you can always jump to the first match with `<CR>`
-				current = true,
-				-- show the label after the match
-				after = true, ---@type boolean|number[]
-				-- show the label before the match
-				before = false, ---@type boolean|number[]
-				-- position of the label extmark
-				style = "overlay", ---@type "eol" | "overlay" | "right_align" | "inline"
-				-- flash tries to re-use labels that were already assigned to a position,
-				-- when typing more characters. By default only lower-case labels are re-used.
-				reuse = "lowercase", ---@type "lowercase" | "all"
-			},
-			-- show a backdrop with hl FlashBackdrop
-			backdrop = true,
-			-- Highlight the search matches
-			matches = true,
-			-- extmark priority
-			priority = 5000,
-			groups = {
-				match = "FlashMatch",
-				current = "FlashCurrent",
-				backdrop = "FlashBackdrop",
-				label = "FlashLabel",
-			},
+		label = {
+			-- add a label for the first match in the current window.
+			-- you can always jump to the first match with `<CR>`
+			current = true,
+			-- show the label after the match
+			after = true, ---@type boolean|number[]
+			-- show the label before the match
+			before = false, ---@type boolean|number[]
+			-- position of the label extmark
+			style = "overlay", ---@type "eol" | "overlay" | "right_align" | "inline"
+			-- flash tries to re-use labels that were already assigned to a position,
+			-- when typing more characters. By default only lower-case labels are re-used.
+			reuse = "lowercase", ---@type "lowercase" | "all"
+		},
+		-- show a backdrop with hl FlashBackdrop
+		backdrop = true,
+		-- Highlight the search matches
+		matches = true,
+		-- extmark priority
+		priority = 5000,
+		groups = {
+			match = "FlashMatch",
+			current = "FlashCurrent",
+			backdrop = "FlashBackdrop",
+			label = "FlashLabel",
 		},
 		-- action to perform when picking a label.
 		-- defaults to the jumping logic depending on the mode.
