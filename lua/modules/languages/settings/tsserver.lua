b939da156fd453577a6bdb0999c917317ca4172b
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-07-23 21:52:45 -0500

    tsserver

--- lua/modules/languages/settings/tsserver.lua
+++ lua/modules/languages/settings/tsserver.lua
@@ -1,4 +1,5 @@
 return {
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" };
   root_dir = function() return vim.loop.cwd() end
+  -- root_dir = require("lspconfig").util.root_pattern("package.json"),
 }
