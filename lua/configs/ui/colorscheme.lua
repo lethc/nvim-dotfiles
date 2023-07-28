36cb74c1f840d4e111f806f72014c7a9611a83b6
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-07-28 10:14:05 -0500

    mellow

--- lua/configs/ui/colorscheme.lua
+++ lua/configs/ui/colorscheme.lua
@@ -3,7 +3,7 @@ local var_colour = {}
 var_colour.colour = function()
   -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme arctic")
+vim.cmd("colorscheme mellow")
 -- end themery block
 end
 
