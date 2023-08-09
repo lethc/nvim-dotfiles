508e5cc1b20e288b6fa58738bcc79e9a6853967c
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-08-09 09:00:34 -0500

    neovide config

--- init.lua
+++ init.lua
@@ -1,1 +1,17 @@
 require("configs")
+
+if vim.g.neovide then
+	local alpha = function()
+		return string.format("%x", math.floor(255 * vim.g.transparency or 0.9))
+	end
+	vim.o.guifont = "JetBrainsMono Nerd Font:h11" -- Font
+	vim.opt.linespace = 0
+	vim.g.neovide_scale_factor = 1.0
+	vim.g.neovide_padding_top = 0 --padding
+	vim.g.neovide_padding_bottom = 0
+	vim.g.neovide_padding_right = 0
+	vim.g.neovide_padding_left = 0
+	vim.g.neovide_transparency = 0.9 --background
+	vim.g.transparency = 0.8
+	vim.g.neovide_background_color = "#141B1E" .. alpha()
+end
