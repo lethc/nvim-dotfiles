a03312bf3b5a1393fcef367721dd10a86f8488f4
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-10-30 09:20:39 -0500

    TWValues keys added

--- lua/modules/languages/init.lua
+++ lua/modules/languages/init.lua
@@ -122,9 +122,9 @@ local modules = {
   -- },
   {
       "MaximilianLloyd/tw-values.nvim",
-      -- keys = {
-      --     { "<leader>sv", "<cmd>TWValues<cr>", desc = "Show tailwind CSS values" },
-      -- },
+      keys = {
+          { "<leader>lv", "<cmd>TWValues<cr>", desc = "Show tailwind CSS values" },
+      },
       opts = {
           border = "rounded", -- Valid window border style,
           show_unknown_classes = true, -- Shows the unknown classes popup
