5f7483bb50b61587f5c78a9cb3beda3c88900db7
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-11-06 14:58:05 -0500

    update flash configuration

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -88,11 +88,30 @@ local modules = {
     "folke/flash.nvim",
     event = { "InsertEnter" },
     keys = {
+      {
+        "s", mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = { forward = true, wrap = false, multi_window = false },
+          })
+        end, desc = "Flash"
+      },
+      {
+        "S", mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = { forward = false, wrap = false, multi_window = false },
+          })
+        end, desc = "Flash Treesitter"
+      },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "/", mode = { "n" } },
       { "f", mode = { "n" } },
       { "F", mode = { "n" } },
       { "t", mode = { "n" } },
       { "T", mode = { "n" } },
-      { "/", mode = { "n" } },
     },
     config = edit_config.flash_nvim,
   },
