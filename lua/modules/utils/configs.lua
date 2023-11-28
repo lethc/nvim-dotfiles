c4922fb4459dd1b0adc569fd0e8edc8dae04bc13
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-11-28 15:59:04 -0500

    whichkey icon group update

--- lua/modules/utils/configs.lua
+++ lua/modules/utils/configs.lua
@@ -197,7 +197,7 @@ config.which_key = function()
     icons = {
       breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
       separator = "➜", -- symbol used between a key and it's label
-      group = "+", -- symbol prepended to a group
+      group = "", -- symbol prepended to a group
     },
     popup_mappings = {
       scroll_down = "<c-d>", -- binding to scroll down inside the popup
