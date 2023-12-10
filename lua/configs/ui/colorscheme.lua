cae1dc2949d0412fcce669e868b045bb20db316c
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-12-10 12:39:00 -0500

    no-clown-fiesta again

--- lua/configs/ui/colorscheme.lua
+++ lua/configs/ui/colorscheme.lua
@@ -3,8 +3,8 @@ local var_colour = {}
 var_colour.colour = function()
   -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme tokyonight")
-vim.g.theme_id = 3
+vim.cmd("colorscheme no-clown-fiesta")
+vim.g.theme_id = 1
 -- end themery block
 end
 
