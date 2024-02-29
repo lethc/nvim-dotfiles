a78cb4cb9c15ebf122eb2ab7f9829e2078b4f8ac
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-02-28 23:34:09 -0500

    Add wrap keymaps

--- lua/configs/base/keymaps.lua
+++ lua/configs/base/keymaps.lua
@@ -31,6 +31,8 @@ keymaps["normal"] = {
     { "<leader>u", "mzlblgueh~`z" }, -- Quickly Toggle Casing
     { "gD", "<cmd>lua vim.lsp.buf.definition()<CR>" },
     { "gl", "<cmd>lua vim.lsp.buf.declaration()<CR>" },
+    { "<leader>hn", "<cmd>set nowrap<CR>" },
+    { "<leader>hw", "<cmd>set wrap<CR>" },
 }
 
 keymaps["insert"] = {
