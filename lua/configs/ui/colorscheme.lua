8a62101bd78fbb47be0bdbe63a823a12fa2f1c3b
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-02-28 22:18:36 -0500

    ...

--- lua/configs/ui/colorscheme.lua
+++ lua/configs/ui/colorscheme.lua
@@ -2,10 +2,10 @@ local var_colour = {}
 
 var_colour.colour = function()
     -- Themery block
-    -- This block will be replaced by Themery.
-    vim.cmd("colorscheme canon")
-    vim.g.theme_id = 2
-    -- end themery block
+-- This block will be replaced by Themery.
+vim.cmd("colorscheme canon")
+vim.g.theme_id = 2
+-- end themery block
 end
 
 return var_colour
