e2ea60def417a3c46a0bbd5d86e9220bce431d4c
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-03-01 03:28:04 -0500

    Add plugin to interact with Yazi

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -426,5 +426,71 @@ local modules = {
             })
         end,
     },
+    {
+        "rolv-apneseth/tfm.nvim",
+        lazy = false,
+        opts = {
+            -- TFM to use
+            -- Possible choices: "ranger" | "nnn" | "lf" | "vifm" | "yazi" (default)
+            file_manager = "yazi",
+            -- Replace netrw entirely
+            -- Default: false
+            replace_netrw = true,
+            -- Enable creation of commands
+            -- Default: false
+            -- Commands:
+            --   Tfm: selected file(s) will be opened in the current window
+            --   TfmSplit: selected file(s) will be opened in a horizontal split
+            --   TfmVsplit: selected file(s) will be opened in a vertical split
+            --   TfmTabedit: selected file(s) will be opened in a new tab page
+            enable_cmds = false,
+            -- Custom keybindings only applied within the TFM buffer
+            -- Default: {}
+            keybindings = {
+                ["<ESC>"] = "q"
+            },
+            -- Customise UI. The below options are the default
+            ui = {
+                border = "rounded",
+                height = 1,
+                width = 1,
+                x = 0.5,
+                y = 0.5,
+            },
+        },
+        keys = {
+            {
+                "<leader>y",
+                function()
+                    require("tfm").open()
+                end,
+                desc = "TFM",
+            },
+            {
+                "<leader>es",
+                function()
+                    local tfm = require("tfm")
+                    tfm.open(nil, tfm.OPEN_MODE.split)
+                end,
+                desc = "TFM - horizonal split",
+            },
+            {
+                "<leader>ev",
+                function()
+                    local tfm = require("tfm")
+                    tfm.open(nil, tfm.OPEN_MODE.vsplit)
+                end,
+                desc = "TFM - vertical split",
+            },
+            {
+                "<leader>et",
+                function()
+                    local tfm = require("tfm")
+                    tfm.open(nil, tfm.OPEN_MODE.tabedit)
+                end,
+                desc = "TFM - new tab",
+            },
+        },
+    }
 }
 return modules
