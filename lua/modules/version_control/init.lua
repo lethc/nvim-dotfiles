99e74b30f83555fe326578411e122009cb433c68
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-03-15 14:00:59 -0500

    Add Neogit plugin
    This is a really interesting plugin

--- lua/modules/version_control/init.lua
+++ lua/modules/version_control/init.lua
@@ -19,5 +19,17 @@ local modules = {
         },
         config = function() end,
     },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            -- "nvim-lua/plenary.nvim", -- required
+            -- "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed, not both.
+            -- "nvim-telescope/telescope.nvim", -- optional
+            -- "ibhagwan/fzf-lua", -- optional
+        },
+        config = true,
+    },
 }
 return modules
