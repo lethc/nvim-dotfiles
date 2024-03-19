a34e48fd63d895679e77397e1d5cc0fa454e8ebd
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-03-19 15:16:38 -0500

    Update clipboard image path

--- lua/modules/completion/configs.lua
+++ lua/modules/completion/configs.lua
@@ -416,15 +416,15 @@ config.clipboard_image = function()
     -- end
     require("clipboard-image").setup({
         default = {
-            img_dir = "/home/Lummyn/Home/.assets/img",
+            img_dir = "/home/Lummyn/Home/assets/img",
             img_name = function()
                 return os.date("%Y-%m-%d-%H-%M-%S")
             end, -- Example result: "2021-04-13-10-04-18"
             affix = "<\n  %s\n>", -- Multi lines affix
         },
         markdown = {
-            img_dir = { "/home/Lummyn/Home/.assets/img" }, -- Use table for nested dir (New feature form PR #20)
-            img_dir_txt = "/.assets/img",
+            img_dir = { "/home/Lummyn/Home/assets/img" }, -- Use table for nested dir (New feature form PR #20)
+            img_dir_txt = "assets/img",
             -- img_handler = function(img) -- New feature from PR #22
             -- 	local script = string.format('./image_compressor.sh "%s"', img.path)
             -- 	os.execute(script)
