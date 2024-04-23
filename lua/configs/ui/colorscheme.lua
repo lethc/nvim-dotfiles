7e5694db025a1c8072a60e6a5a06b376eb6f3171
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-04-23 00:03:41 -0500

    Update colorscheme config

--- lua/configs/ui/colorscheme.lua
+++ lua/configs/ui/colorscheme.lua
@@ -1,7 +1,6 @@
 local var_colour = {}
 
 var_colour.colour = function()
-vim.cmd([[ hi! CursorLineBG guibg=#373737 guifg=#E1E1E1 ]])
     -- Themery block
 -- This block will be replaced by Themery.
 vim.cmd("colorscheme canon")
