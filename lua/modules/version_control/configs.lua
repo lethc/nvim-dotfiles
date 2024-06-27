17547a470ff0c2951e437ac92b93f94a5483c4ad
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-06-27 13:22:38 -0500

    Update gitsigns configuration

--- lua/modules/version_control/configs.lua
+++ lua/modules/version_control/configs.lua
@@ -8,31 +8,11 @@ config.gitsigns_nvim = function()
     end
     gitsigns.setup({
         signs = {
-            add = { hl = "GitSignsAdd", text = " ▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-            change = {
-                hl = "GitSignsChange",
-                text = " ▎",
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn",
-            },
-            delete = {
-                hl = "GitSignsDelete",
-                text = " 󰐊",
-                numhl = "GitSignsDeleteNr",
-                linehl = "GitSignsDeleteLn",
-            },
-            topdelete = {
-                hl = "GitSignsDelete",
-                text = " 󰐊",
-                numhl = "GitSignsDeleteNr",
-                linehl = "GitSignsDeleteLn",
-            },
-            changedelete = {
-                hl = "GitSignsChange",
-                text = " ▎",
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn",
-            },
+            add = { text = " ▎", },
+            change = { text = " ▎", },
+            delete = { text = " 󰐊", },
+            topdelete = { text = " 󰐊", },
+            changedelete = { text = " ▎", },
             untracked = { text = " ▎" },
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -40,20 +20,19 @@ config.gitsigns_nvim = function()
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
-            interval = 1000,
             follow_files = true,
         },
-        attach_to_untracked = true,
+        auto_attach = true,
+        attach_to_untracked = false,
         current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
             virt_text = true,
             virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
             delay = 1000,
             ignore_whitespace = false,
+            virt_text_priority = 100,
         },
-        current_line_blame_formatter_opts = {
-            relative_time = false,
-        },
+        current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
@@ -65,9 +44,6 @@ config.gitsigns_nvim = function()
             relative = "cursor",
             row = 0,
             col = 1,
-        },
-        yadm = {
-            enable = false,
         },
     })
 end
