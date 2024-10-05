247caa415d87efb5d2b4df08d5d2364265c22b12
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-05 08:35:11 -0500

    update

--- lua/configs/core/lazy.lua
+++ lua/configs/core/lazy.lua
@@ -23,7 +23,7 @@ lazy["load"] = function()
     require("lazy").setup(modules, {
         install = {
             missing = true,
-            colorscheme = { "canon", "tokyonight", "lvim", "habamax" },
+            colorscheme = { "canon-onedark", "tokyonight", "lvim", "habamax" },
         },
         ui = {
             size = {
