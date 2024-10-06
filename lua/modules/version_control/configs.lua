682df23a05af592fc5b5c1addcc1563c448895e8
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-06 11:54:01 -0500

    Update gitsigns configuration

--- lua/modules/version_control/configs.lua
+++ lua/modules/version_control/configs.lua
@@ -8,11 +8,19 @@ config.gitsigns_nvim = function()
     end
     gitsigns.setup({
         signs = {
-            add = { text = " ▎", },
-            change = { text = " ▎", },
-            delete = { text = " 󰐊", },
-            topdelete = { text = " 󰐊", },
-            changedelete = { text = " ▎", },
+            add = { text = " ▎" },
+            change = { text = " ▎" },
+            delete = { text = " 󰐊" },
+            topdelete = { text = " 󰐊" },
+            changedelete = { text = " ▎" },
+            untracked = { text = " ▎" },
+        },
+        signs_staged = {
+            add = { text = " ▎" },
+            change = { text = " ▎" },
+            delete = { text = " 󰐊" },
+            topdelete = { text = " 󰐊" },
+            changedelete = { text = " ▎" },
             untracked = { text = " ▎" },
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -31,6 +39,7 @@ config.gitsigns_nvim = function()
             delay = 1000,
             ignore_whitespace = false,
             virt_text_priority = 100,
+            use_focus = true,
         },
         current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
         sign_priority = 6,
