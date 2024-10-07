b283f006e07acf5b8987b3adbc8f958cf42ac55b
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-07 10:53:26 -0500

    Enable Blur in neovide
    It looks better on certain themes

--- lua/configs/init.lua
+++ lua/configs/init.lua
@@ -32,26 +32,26 @@ if vim.g.neovide then
 
     -- padding
     vim.g.neovide_padding_top = 5
-    vim.g.neovide_padding_left = 4
+    vim.g.neovide_padding_left = 5
     -- vim.g.neovide_padding_bottom = 0
     -- vim.g.neovide_padding_right = 0
 
     -- disable blur
-    vim.g.neovide_window_blurred = false
-    vim.g.neovide_floating_blur_amount_x = 0.0
-    vim.g.neovide_floating_blur_amount_y = 0.0
-    vim.g.neovide_floating_shadow = false
-    vim.g.neovide_floating_z_height = 0
-    vim.g.neovide_light_angle_degrees = 0
-    vim.g.neovide_light_radius = 0
-
-    vim.g.neovide_transparency = 1.0
-    -- vim.g.neovide_transparency = 0.8 --background
-    vim.g.neovide_hide_mouse_when_typing = true
-    vim.g.neovide_underline_stroke_scale = 0.5
-    vim.g.neovide_refresh_rate = 60
-    vim.g.neovide_fullscreen = false
-    vim.g.neovide_remember_window_size = false
+    -- vim.g.neovide_window_blurred = false
+    -- vim.g.neovide_floating_blur_amount_x = 0.0
+    -- vim.g.neovide_floating_blur_amount_y = 0.0
+    -- vim.g.neovide_floating_shadow = false
+    -- vim.g.neovide_floating_z_height = 0
+    -- vim.g.neovide_light_angle_degrees = 0
+    -- vim.g.neovide_light_radius = 0
+    --
+    -- vim.g.neovide_transparency = 1.0
+    -- -- vim.g.neovide_transparency = 0.8 --background
+    -- vim.g.neovide_hide_mouse_when_typing = true
+    -- vim.g.neovide_underline_stroke_scale = 0.5
+    -- vim.g.neovide_refresh_rate = 60
+    -- vim.g.neovide_fullscreen = false
+    -- vim.g.neovide_remember_window_size = false
 end
 
 function Cutlinetotodolist()
