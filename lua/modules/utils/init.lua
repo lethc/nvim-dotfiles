8d77c1f8ec01004125833a27294dc6ed769c725f
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-23 15:09:53 -0500

    Disable emoji.nvim

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -903,28 +903,6 @@ local modules = {
         "csessh/stopinsert.nvim",
         opts = {},
     },
-    {
-        "allaman/emoji.nvim",
-        version = "1.0.0", -- optionally pin to a tag
-        ft = "markdown", -- adjust to your needs
-        dependencies = {
-            -- optional for telescope integration
-            "nvim-telescope/telescope.nvim",
-        },
-        opts = {
-            -- default is false
-            enable_cmp_integration = true,
-            -- optional if your plugin installation directory
-            -- is not vim.fn.stdpath("data") .. "/lazy/
-            -- plugin_path = vim.fn.expand("$HOME/plugins/"),
-        },
-        config = function(_, opts)
-            require("emoji").setup(opts)
-            -- optional for telescope integration
-            local ts = require("telescope").load_extension("emoji")
-            vim.keymap.set("n", "<leader>se", ts.emoji, { desc = "[S]earch [E]moji" })
-        end,
-    },
     -- {
     --     "EL-MASTOR/bufferlist.nvim",
     --     lazy = true,
