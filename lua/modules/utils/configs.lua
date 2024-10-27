2b294fffeffc3b41babd6ccc44d1df930125062f
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-27 08:38:20 -0500

    Update noteID for markdown files created with obsidian.nvim

--- lua/modules/utils/configs.lua
+++ lua/modules/utils/configs.lua
@@ -1102,6 +1102,24 @@ config.obsidian_nvim = function()
         picker = {
             name = "fzf-lua",
         },
+
+        -- Optional, customize how note IDs are generated given an optional title.
+        note_id_func = function(title)
+            -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+            -- In this case a note with the title 'My new note' will be given an ID that looks
+            -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+            local suffix = ""
+            if title ~= nil then
+                -- If title is given, transform it into valid file name.
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            else
+                -- If title is nil, just add 4 random uppercase letters to the suffix.
+                for _ = 1, 4 do
+                    suffix = suffix .. string.char(math.random(65, 90))
+                end
+            end
+            return tostring(os.time()) .. "-" .. suffix
+        end,
     })
     vim.keymap.set("n", "gf", function()
         if require("obsidian").util.cursor_on_markdown_link() then
