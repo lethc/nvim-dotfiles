97bae0f757e1c33268b003089e70d30983ad8669
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-27 08:41:22 -0500

    Add a plugin to autoformat tables in markdown

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -933,5 +933,19 @@ local modules = {
             "nvim-tree/nvim-web-devicons",
         },
     },
+    {
+        "Kicamon/markdown-table-mode.nvim",
+        config = function()
+            require("markdown-table-mode").setup({
+                filetype = {
+                    "*.md",
+                },
+                options = {
+                    insert = true, -- when typeing "|"
+                    insert_leave = true, -- when leaveing insert
+                },
+            })
+        end,
+    },
 }
 return modules
