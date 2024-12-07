939b122ce1e388dd1b11f8634b2fc3cdedcf10a2
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-12-07 02:02:21 -0500

    Enable image.nvim

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -312,48 +312,48 @@ local modules = {
             })
         end,
     },
-    -- {
-    --     "3rd/image.nvim", -- Unfortunately it's not working with zellij https://github.com/3rd/image.nvim/issues/161
-    --     config = function()
-    --         -- default config
-    --         require("image").setup({
-    --             backend = "ueberzug",
-    --             integrations = {
-    --                 markdown = {
-    --                     enabled = true,
-    --                     clear_in_insert_mode = false,
-    --                     download_remote_images = true,
-    --                     only_render_image_at_cursor = false,
-    --                     filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-    --                 },
-    --                 neorg = {
-    --                     enabled = true,
-    --                     clear_in_insert_mode = false,
-    --                     download_remote_images = true,
-    --                     only_render_image_at_cursor = false,
-    --                     filetypes = { "norg" },
-    --                 },
-    --                 html = {
-    --                     enabled = false,
-    --                 },
-    --                 css = {
-    --                     enabled = false,
-    --                 },
-    --             },
-    --             max_width = nil,
-    --             max_height = nil,
-    --             max_width_window_percentage = nil,
-    --             max_height_window_percentage = 50,
-    --             window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-    --             window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-    --             editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-    --             tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-    --             hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
-    --         })
-    --         package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
-    --         package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
-    --     end,
-    -- },
+    {
+        "3rd/image.nvim", -- zellij issues https://github.com/3rd/image.nvim/issues/161
+        config = function()
+            -- default config
+            require("image").setup({
+                backend = "ueberzug",
+                integrations = {
+                    markdown = {
+                        enabled = true,
+                        clear_in_insert_mode = false,
+                        download_remote_images = true,
+                        only_render_image_at_cursor = false,
+                        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+                    },
+                    neorg = {
+                        enabled = true,
+                        clear_in_insert_mode = false,
+                        download_remote_images = true,
+                        only_render_image_at_cursor = false,
+                        filetypes = { "norg" },
+                    },
+                    html = {
+                        enabled = false,
+                    },
+                    css = {
+                        enabled = false,
+                    },
+                },
+                max_width = nil,
+                max_height = nil,
+                max_width_window_percentage = nil,
+                max_height_window_percentage = 50,
+                window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+                window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+                editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+                tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+                hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+            })
+            package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
+            package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
+        end,
+    },
     {
         "OXY2DEV/markview.nvim",
         ft = "markdown",
@@ -575,7 +575,6 @@ local modules = {
     },
     {
         "backdround/global-note.nvim",
-        event = "insertEnter",
         config = edit_config.global_note,
     },
     {
