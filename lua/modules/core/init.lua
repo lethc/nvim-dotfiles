b6dc19f629553bde1995f75d46558b28199d8802
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-12-15 00:57:37 -0500

    Add plugin to move trough text objects

--- lua/modules/core/init.lua
+++ lua/modules/core/init.lua
@@ -350,5 +350,17 @@ local modules = {
             })
         end,
     },
+    {
+        "aaronik/treewalker.nvim",
+        config = function()
+            require("treewalker").setup({
+                highlight = true,
+            })
+            vim.api.nvim_set_keymap("n", "<A-j>", ":Treewalker Down<CR>", { noremap = true })
+            vim.api.nvim_set_keymap("n", "<A-k>", ":Treewalker Up<CR>", { noremap = true })
+            vim.api.nvim_set_keymap("n", "<A-h>", ":Treewalker Left<CR>", { noremap = true })
+            vim.api.nvim_set_keymap("n", "<A-l>", ":Treewalker Right<CR>", { noremap = true })
+        end,
+    },
 }
 return modules
