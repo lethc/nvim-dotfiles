d118ae60c56133a5d05783c32c30c8517ce7682f
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-12-15 00:57:10 -0500

    Disable focus nvim

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -323,7 +323,7 @@ local modules = {
                         enabled = true,
                         clear_in_insert_mode = false,
                         download_remote_images = true,
-                        only_render_image_at_cursor = false,
+                        only_render_image_at_cursor = true,
                         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
                     },
                     neorg = {
@@ -840,72 +840,72 @@ local modules = {
             ]])
         end,
     },
-    {
-        "nvim-focus/focus.nvim",
-        event = "BufReadPost",
-        version = "*",
-        config = function()
-            require("focus").setup({
-                enable = true, -- Enable module
-                commands = true, -- Create Focus commands
-                autoresize = {
-                    enable = true, -- Enable or disable auto-resizing of splits
-                    width = 0, -- Force width for the focused window
-                    height = 0, -- Force height for the focused window
-                    minwidth = 0, -- Force minimum width for the unfocused window
-                    minheight = 0, -- Force minimum height for the unfocused window
-                    height_quickfix = 10, -- Set the height of quickfix panel
-                },
-                split = {
-                    bufnew = false, -- Create blank buffer for new split windows
-                    tmux = false, -- Create tmux splits instead of neovim splits
-                },
-                ui = {
-                    number = false, -- Display line numbers in the focussed window only
-                    relativenumber = false, -- Display relative line numbers in the focussed window only
-                    hybridnumber = false, -- Display hybrid line numbers in the focussed window only
-                    absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
-
-                    cursorline = true, -- Display a cursorline in the focussed window only
-                    cursorcolumn = false, -- Display cursorcolumn in the focussed window only
-                    colorcolumn = {
-                        enable = false, -- Display colorcolumn in the foccused window only
-                        list = "+1", -- Set the comma-saperated list for the colorcolumn
-                    },
-                    signcolumn = true, -- Display signcolumn in the focussed window only
-                    winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
-                },
-            })
-            local ignore_filetypes = { "neo-tree" }
-            local ignore_buftypes = { "nofile", "prompt", "popup" }
-
-            local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
-
-            vim.api.nvim_create_autocmd("WinEnter", {
-                group = augroup,
-                callback = function(_)
-                    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-                        vim.w.focus_disable = true
-                    else
-                        vim.w.focus_disable = false
-                    end
-                end,
-                desc = "Disable focus autoresize for BufType",
-            })
-
-            vim.api.nvim_create_autocmd("FileType", {
-                group = augroup,
-                callback = function(_)
-                    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-                        vim.b.focus_disable = true
-                    else
-                        vim.b.focus_disable = false
-                    end
-                end,
-                desc = "Disable focus autoresize for FileType",
-            })
-        end,
-    },
+    -- {
+    --     "nvim-focus/focus.nvim",
+    --     event = "BufReadPost",
+    --     version = "*",
+    --     config = function()
+    --         require("focus").setup({
+    --             enable = true, -- Enable module
+    --             commands = true, -- Create Focus commands
+    --             autoresize = {
+    --                 enable = true, -- Enable or disable auto-resizing of splits
+    --                 width = 0, -- Force width for the focused window
+    --                 height = 0, -- Force height for the focused window
+    --                 minwidth = 0, -- Force minimum width for the unfocused window
+    --                 minheight = 0, -- Force minimum height for the unfocused window
+    --                 height_quickfix = 10, -- Set the height of quickfix panel
+    --             },
+    --             split = {
+    --                 bufnew = false, -- Create blank buffer for new split windows
+    --                 tmux = false, -- Create tmux splits instead of neovim splits
+    --             },
+    --             ui = {
+    --                 number = false, -- Display line numbers in the focussed window only
+    --                 relativenumber = false, -- Display relative line numbers in the focussed window only
+    --                 hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+    --                 absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+    --
+    --                 cursorline = true, -- Display a cursorline in the focussed window only
+    --                 cursorcolumn = false, -- Display cursorcolumn in the focussed window only
+    --                 colorcolumn = {
+    --                     enable = false, -- Display colorcolumn in the foccused window only
+    --                     list = "+1", -- Set the comma-saperated list for the colorcolumn
+    --                 },
+    --                 signcolumn = true, -- Display signcolumn in the focussed window only
+    --                 winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+    --             },
+    --         })
+    --         local ignore_filetypes = { "neo-tree" }
+    --         local ignore_buftypes = { "nofile", "prompt", "popup" }
+    --
+    --         local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+    --
+    --         vim.api.nvim_create_autocmd("WinEnter", {
+    --             group = augroup,
+    --             callback = function(_)
+    --                 if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+    --                     vim.w.focus_disable = true
+    --                 else
+    --                     vim.w.focus_disable = false
+    --                 end
+    --             end,
+    --             desc = "Disable focus autoresize for BufType",
+    --         })
+    --
+    --         vim.api.nvim_create_autocmd("FileType", {
+    --             group = augroup,
+    --             callback = function(_)
+    --                 if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+    --                     vim.b.focus_disable = true
+    --                 else
+    --                     vim.b.focus_disable = false
+    --                 end
+    --             end,
+    --             desc = "Disable focus autoresize for FileType",
+    --         })
+    --     end,
+    -- },
     -- {
     --     "kevinhwang91/nvim-hlslens", -- Show number of similar matches found when searching for a word (noice can do this too)
     --     config = function()
