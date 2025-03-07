130495e7951eba491c3c2b2fa28685b670414996
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-03-07 14:52:46 -0500

    Update table-mode plugin

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -961,7 +961,6 @@ local modules = {
     },
     {
         "Kicamon/markdown-table-mode.nvim",
-        ft = "markdown",
         config = function()
             require("markdown-table-mode").setup({
                 filetype = {
@@ -970,6 +969,8 @@ local modules = {
                 options = {
                     insert = true, -- when typeing "|"
                     insert_leave = true, -- when leaveing insert
+                    pad_separator_line = false, -- add space in separator line
+                    alig_style = "default", -- default, left, center, right
                 },
             })
         end,
