ac61db7b5aca5d6faf7d8b4ab41840006dfbdd40
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-03-23 21:46:08 -0500

    Add function to open links with is-fast tui browser

--- lua/configs/init.lua
+++ lua/configs/init.lua
@@ -76,6 +76,29 @@ if vim.g.neovide then
     -- vim.g.neovide_remember_window_size = false
 end
 
+-- Open url links in is-fast browser inside neovim
+
+vim.keymap.set("n", "gX", function()
+  local line = vim.fn.getline(".")  -- Get the current line
+  local col = vim.fn.col(".")       -- Get cursor position in the line
+
+  -- Match a Markdown-style link [text](URL) using Lua pattern matching
+  local before_cursor = line:sub(1, col)  -- Text before the cursor
+  local after_cursor = line:sub(col)      -- Text after the cursor
+
+  -- Find the nearest URL pattern `(URL)`
+  local url = before_cursor:match("%b()") or after_cursor:match("%b()")
+
+  if url then
+    url = url:sub(2, -2) -- Remove surrounding parentheses
+    vim.cmd("startinsert | tabnew | set norelativenumber | set nonumber | terminal is-fast -d \"" .. url .. "\"")
+  else
+    print("No valid URL found")
+  end
+end, { noremap = true, silent = true })
+
+-- Special Funtion for my Todolist
+
 function Cutlinetotodolist()
     -- Get the current line content
     local current_line = vim.api.nvim_get_current_line()
