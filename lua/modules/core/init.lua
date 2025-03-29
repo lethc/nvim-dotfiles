ecda7e6a8dbe4461fac424e743e43d1d2f0d64aa
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-03-29 17:09:09 -0500

    Update dashboard actions

--- lua/modules/core/init.lua
+++ lua/modules/core/init.lua
@@ -99,13 +99,13 @@ local modules = {
                 },
                 { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
                 { icon = " ", key = "F", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                {
-                    icon = "󱎱 ",
-                    key = "h",
-                    desc = "Home",
-                    action = ":lua Snacks.dashboard.pick('files', {cwd = '" .. home .. "/Home'})",
-                },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                -- {
+                --     icon = "󱎱 ",
+                --     key = "h",
+                --     desc = "Home",
+                --     action = ":lua Snacks.dashboard.pick('files', {cwd = '" .. home .. "/Home'})",
+                -- },
                 -- {
                 --     icon = " ",
                 --     key = "o",
@@ -120,7 +120,7 @@ local modules = {
                 },
                 { icon = " ", key = "s", desc = "Restore Session", section = "session" },
                 -- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-                -- { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
             }
             return {
                 bigfile = { enabled = true },
