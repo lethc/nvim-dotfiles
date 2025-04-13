7a574be49c95d6a05471ebd4c9a0dec0fc97ddd7
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-04-13 10:25:27 -0500

    Update statuscol.nvim configuration

--- lua/modules/ui/init.lua
+++ lua/modules/ui/init.lua
@@ -356,28 +356,39 @@ local modules = {
         opts = function()
             local builtin = require("statuscol.builtin")
             return {
-                ft_ignore = { "neo-tree", "neo-tree-popup", "alpha", "lazy", "mason", "dashboard" },
+                ft_ignore = {
+                    "neo-tree",
+                    "neo-tree-popup",
+                    "alpha",
+                    "lazy",
+                    "mason",
+                    "dashboard",
+                    "help",
+                    "vim",
+                    "alpha",
+                    "toggleterm",
+                    "noice",
+                    "Trouble",
+                },
                 relculright = true,
                 segments = {
-                    {
-                        sign = {
-                            text = { " " },
-                            maxwidth = 1,
-                            colwidth = 2,
-                            auto = false,
-                        },
-                    },
+                    { text = { "  " }, click = "v:lua.ScSa" },
                     { text = { builtin.foldfunc, "" }, click = "v:lua.ScFa" },
-                    { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+                    { text = { builtin.lnumfunc, "  " }, click = "v:lua.ScLa" },
                     {
-                        sign = {
-                            text = { " " },
-                            maxwidth = 1,
-                            colwidth = 1,
-                            auto = false,
-                        },
+                        text = { "%s" },
+                        click = "v:lua.ScSa",
+                        auto = false,
                     },
-                    { text = { "%s" }, click = "v:lua.ScSa" },
+                    -- {
+                    --     -- Dap Breakpoints
+                    --     sign = { name = { "Dap" }, maxwidth = 1, colwidth = 2, auto = true, fillchars = "" },
+                    --     click = "v:lua.ScSa",
+                    -- },
+                    -- {
+                    --     sign = { namespace = { "gitsign+" }, maxwidth = 1, colwidth = 1, auto = false, fillchar = " " },
+                    --     click = "v:lua.ScSa",
+                    -- },
                 },
             }
         end,
