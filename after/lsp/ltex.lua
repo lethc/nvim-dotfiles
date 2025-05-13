1dda0e973eb4262a657f00bc909975364e235e9c
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-05-13 12:30:14 -0500

    Update lsp settings

--- after/lsp/ltex.lua
+++ after/lsp/ltex.lua
@@ -6,7 +6,24 @@ return {
             -- dictionary = {"en-GB":["nvim", "linux"]},
             -- In markdown file add lang: es, https://valentjn.github.io/ltex/advanced-usage.html#magic-comments
         },
+        enabled = {
+            "bibtex",
+            "gitcommit",
+            "markdown",
+            "org",
+            "tex",
+            "restructuredtext",
+            "rsweave",
+            "latex",
+            "quarto",
+            "rmd",
+            "context",
+            "html",
+            "xhtml",
+            "mail",
+            "plaintext",
+        },
     },
     -- filetypes = { "bib", "gitcommit", "org", "plaintex", "rst", "rnoweb", "tex", "pandoc", "markdown" }
-    filetypes = { "org" },
+    filetypes = { "org", "latex" },
 }
