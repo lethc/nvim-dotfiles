216353e7ffcabc87fa1032f379ce6a0a549d1d3a
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-07-16 13:09:59 -0500

    update

--- lua/configs/base/init.lua
+++ lua/configs/base/init.lua
@@ -74,10 +74,10 @@ configs["auto_commands"] = function()
 
   augroup _markdown
     autocmd!
-    autocmd FileType markdown setlocal nowrap
+    "autocmd FileType markdown setlocal nowrap
     autocmd FileType markdown setlocal spell
-    "autocmd FileType markdown setlocal textwidth=116 "Default autoindent = true
-    autocmd FileType markdown setlocal textwidth=90 "Default autoindent = true
+    autocmd FileType markdown setlocal textwidth=116 "Default autoindent = true
+    "autocmd FileType markdown setlocal textwidth=90 "Default autoindent = true
     autocmd FileType markdown setlocal shiftwidth=2 "When press tab avoid 4-space indent
   augroup end
 
