d23736db1d80fcc434b0491bc885aa034741ad27
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-08-26 12:45:42 -0500

    Add onedark colorscheme

--- lua/modules/ui/init.lua
+++ lua/modules/ui/init.lua
@@ -1,6 +1,29 @@
 local ui_config = require("modules.ui.configs")
 local modules = {
     {
+        "olimorris/onedarkpro.nvim",
+        priority = 1000, -- Ensure it loads first
+        config = function()
+            require("onedarkpro").setup({
+                styles = {
+                    types = "NONE",
+                    methods = "NONE",
+                    numbers = "NONE",
+                    strings = "NONE",
+                    comments = "italic",
+                    keywords = "bold,italic",
+                    constants = "NONE",
+                    functions = "italic",
+                    operators = "NONE",
+                    variables = "NONE",
+                    parameters = "NONE",
+                    conditionals = "italic",
+                    virtual_text = "NONE",
+                },
+            })
+        end,
+    },
+    {
         "nyoom-engineering/oxocarbon.nvim",
         priority = 1000,
         config = function()
