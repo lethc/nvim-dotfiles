d5f4bce496afe82e7f3ce963858caa45b29e7f66
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2025-09-22 16:47:26 -0500

    Disable normal-cmdline

--- lua/modules/ui/init.lua
+++ lua/modules/ui/init.lua
@@ -633,31 +633,31 @@ local modules = {
             vim.g.minimap_auto_start_win_enter = 1
         end,
     },
-    {
-        "jake-stewart/normal-cmdline.nvim",
-        event = "CmdlineEnter",
-        config = function()
-            -- make the cmdline insert mode a beam
-            vim.opt.guicursor:append("ci:ver1,c:ver1")
-
-            local cmd = require("normal-cmdline")
-            cmd.setup({
-                -- key to hit within cmdline to enter normal mode:
-                key = "<esc>",
-                -- the cmdline text highlight when in normal mode:
-                hl = "Normal",
-                -- these mappings only apply to normal mode in cmdline:
-                mappings = {
-                    ["k"] = cmd.history.prev,
-                    ["j"] = cmd.history.next,
-                    ["<cr>"] = cmd.accept,
-                    ["<esc>"] = cmd.cancel,
-                    ["<c-c>"] = cmd.cancel,
-                    [":"] = cmd.reset,
-                },
-            })
-        end,
-    },
+    -- {
+    --     "jake-stewart/normal-cmdline.nvim",
+    --     event = "CmdlineEnter",
+    --     config = function()
+    --         -- make the cmdline insert mode a beam
+    --         vim.opt.guicursor:append("ci:ver1,c:ver1")
+    --
+    --         local cmd = require("normal-cmdline")
+    --         cmd.setup({
+    --             -- key to hit within cmdline to enter normal mode:
+    --             key = "<esc>",
+    --             -- the cmdline text highlight when in normal mode:
+    --             hl = "Normal",
+    --             -- these mappings only apply to normal mode in cmdline:
+    --             mappings = {
+    --                 ["k"] = cmd.history.prev,
+    --                 ["j"] = cmd.history.next,
+    --                 ["<cr>"] = cmd.accept,
+    --                 ["<esc>"] = cmd.cancel,
+    --                 ["<c-c>"] = cmd.cancel,
+    --                 [":"] = cmd.reset,
+    --             },
+    --         })
+    --     end,
+    -- },
 }
 
 return modules
