6903817ed376a454af7570857c81f8796890d26a
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2026-03-19 10:13:21 -0500

    Disable zellij keybinds in knvim

--- lua/modules/utils/init.lua
+++ lua/modules/utils/init.lua
@@ -709,19 +709,19 @@ local modules = {
         event = "LspAttach",
         opts = {},
     },
-    {
-        "https://github.com/fresh2dev/zellij.vim.git",
-        lazy = false,
-        config = function()
-            vim.cmd([[
-                let g:zellij_navigator_no_default_mappings = 1
-                nnoremap <silent> <C-h> :ZellijNavigateLeft<CR>
-                nnoremap <silent> <C-j> :ZellijNavigateDown<CR>
-                nnoremap <silent> <C-k> :ZellijNavigateUp<CR>
-                nnoremap <silent> <C-l> :ZellijNavigateRight<CR>
-            ]])
-        end,
-    },
+    -- {
+    --     "https://github.com/fresh2dev/zellij.vim.git",
+    --     lazy = false,
+    --     config = function()
+    --         vim.cmd([[
+    --             let g:zellij_navigator_no_default_mappings = 1
+    --             nnoremap <silent> <C-h> :ZellijNavigateLeft<CR>
+    --             nnoremap <silent> <C-j> :ZellijNavigateDown<CR>
+    --             nnoremap <silent> <C-k> :ZellijNavigateUp<CR>
+    --             nnoremap <silent> <C-l> :ZellijNavigateRight<CR>
+    --         ]])
+    --     end,
+    -- },
     {
         "jackMort/tide.nvim",
         -- event = { "InsertEnter" },
